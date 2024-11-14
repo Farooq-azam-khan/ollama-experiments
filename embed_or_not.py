af7bb84c6eab028d2ee6a3830db07219a3636810
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-11-14 01:10:55 -0500

    long context causes issues on CPU

--- embed_or_not.py
+++ embed_or_not.py
@@ -1,21 +1,42 @@
 import asyncio
 import ollama
+from tqdm import tqdm
 
 print("Reading Book...")
-book = ""
-with open("crime and punishment.txt") as f:
-    book = f.read()
-
-preface = book[: book.find("CHAPTER I")]
-chapter1 = book[book.find("CHAPTER I") : book.find("CHAPTER II")][:1000]
-
-print("Embedding Text...")
-# preface_embeds = ollama.embed(model="nomic-embed-text", prompt=preface)
-# chapter1_embeds = ollama.embeddings(model="nomic-embed-text", prompt=chapter1)
-# print(f'{len(chapter1_embeds)}')
 
 
-async def chat():
+def get_book():
+    book = ""
+    with open("crime and punishment.txt") as f:
+        book = f.read()
+        return book
+
+
+book = get_book()
+
+
+def organize_book(book):
+    parts = ["\n\n\nPART " + part for part in book.split("\n\n\nPART ")]
+    chapters_by_part = [
+        ["\n\n\nCHAPTER " + chapter for chapter in part.split("\n\n\nCHAPTER ")]
+        for part in parts
+    ]
+    return chapters_by_part
+
+
+def embed_organized_book(organized_book):
+    print("Embedding Text...")
+    # preface_embeds = ollama.embed(model="nomic-embed-text", prompt=preface)
+    chapters_by_part_embeds = []
+    for chs in tqdm(organized_book):
+        chs_embeds = []
+        for ch in chs:
+            chs_embeds.append(ollama.embeddings(model="nomic-embed-text", prompt=ch))
+        chapters_by_part_embeds.append(chs_embeds)
+    return chapters_by_part_embeds
+
+
+async def chat(question: str, context: str):
     print("Running inferencing...")
     async for token in await ollama.AsyncClient().chat(
         model="llama3.2:1b-instruct-fp16",
@@ -23,15 +44,27 @@ async def chat():
         messages=[
             {
                 "role": "system",
-                "content": "Help me understand russian literature, but only use the context I provide. Limit yoursel to ten sentences.",
+                "content": """
+Help me understand russian literature, but only use the context I provide. 
+Limit yoursel to ten sentences.
+""".strip()
             },
             {
                 "role": "user",
-                "content": f"# Context\n{chapter1}\n\n# Question\n How does this book start?",
+                "content": f"# Context\n{context}\n\n# Question\n{question}",
             },
         ],
     ):
-        print(token["message"]['content'], flush=True, end="")
+        print(token["message"]["content"], flush=True, end="")
 
 
-asyncio.run(chat())
+if __name__ == "__main__":
+    book = get_book()
+    organized_book = organize_book(book)
+
+    # 1. How does the book start? 
+    # 2. who is the main character?
+    # 3. List the characters here.
+    # 4. What is the setting throughout the chapter?
+    question = input(">> ")
+    asyncio.run(chat(question=question, context=organized_book[1][1]))
